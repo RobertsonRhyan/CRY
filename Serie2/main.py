43693f7ae6ae49dedf4f3b80aac22507c9618350
Rhyan Robertson <49730891+RobertsonRhyan@users.noreply.github.com>
2020-09-28 09:52:21 +0200

    Update main.py

--- Serie2/main.py
+++ Serie2/main.py
@@ -10,5 +10,4 @@ def pgcd(a,b):
 a = 36182736189736918736192873
 b = 131556721028295178496
 
-print("Le pgcd de 132 et 18 est : ", end="")
-print(pgcd(a, b))
+print("Le pgcd de " + str(a) + " et " + str(b) + " = " + str(pgcd(a, b)))
