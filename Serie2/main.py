31ce0a66e26e7b86d32368af6f85cd1f35c3dc2e
Rhyan Robertson <49730891+RobertsonRhyan@users.noreply.github.com>
2020-09-28 09:48:35 +0200

    Create serie2

---
+++ Serie2/main.py
@@ -0,0 +1,14 @@
+
+def pgcd(a,b):
+    if(b == 0):
+        return a
+    else:
+        return pgcd(b, a % b)
+
+
+
+a = 36182736189736918736192873
+b = 131556721028295178496
+
+print("Le pgcd de 132 et 18 est : ", end="")
+print(pgcd(a, b))
